7721a1ac1b1d67e1333041b08900cd9d49dd204e
Salih Terzi <salihterzi@anadolu.edu.tr>
2021-12-17 15:37:54 +0300

    NgxCarouselItems add to NgxCarousel as ContentChildren

--- projects/ngx-carousel/src/lib/ngx-carousel.component.ts
+++ projects/ngx-carousel/src/lib/ngx-carousel.component.ts
@@ -1,21 +1,48 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {NgxCarouselItemComponent} from "./ngx-carousel-item/ngx-carousel-item.component";
+import {
+    AfterContentInit,
+    Component,
+    ContentChildren,
+    forwardRef,
+    QueryList
+} from '@angular/core';
+import { NgxCarouselItemComponent } from './ngx-carousel-item/ngx-carousel-item.component';
+import { interval, Subscriber, Subscription } from 'rxjs';
 
 @Component({
-  selector: 'ngx-carousel',
-  templateUrl: 'ngx-carousel-component.html',
-  styles: []
+    selector: 'ngx-carousel',
+    templateUrl: 'ngx-carousel-component.html',
+    styles: []
 })
-export class NgxCarouselComponent implements OnInit {
-  sliderItems: NgxCarouselItemComponent[] = [];
-  @Input() data:any[];
-  constructor() {
-  }
+export class NgxCarouselComponent implements AfterContentInit {
+    @ContentChildren(forwardRef(() => NgxCarouselItemComponent)) carouselItems: QueryList<NgxCarouselItemComponent>;
+    activeIndex: number = 0;
+    timer: any;
+    duration: number = 3000;
 
-  ngOnInit(): void {
-  }
+    constructor() {
+    }
 
-  addSliderItem(item: NgxCarouselItemComponent) {
-    this.sliderItems.push(item)
-  }
+    skipToNextSlide() {
+        if (this.activeIndex === this.carouselItems.length - 1) {
+            this.activeIndex = 0;
+        } else {
+            this.activeIndex = this.activeIndex + 1;
+        }
+        this.updateCarouselItemsStatus()
+    }
+
+
+
+    updateCarouselItemsStatus() {
+        this.carouselItems.map((element, index) => {
+            element.active = this.activeIndex === index;
+        });
+    }
+
+
+    ngAfterContentInit(): void {
+        // detectChanges doesn't work since property is in another component, so property changes as async
+        Promise.resolve().then(() => this.carouselItems.first.active = true)
+        this.timer = interval(this.duration).subscribe(val => this.skipToNextSlide());
+    }
 }
