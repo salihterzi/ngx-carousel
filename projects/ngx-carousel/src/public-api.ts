b5afbd8694e52cfe4122b1456ac4bd0788d5eaab
salihterzi <salytem@gmail.com>
2023-08-13 23:46:13 +0300

    components converted standalone component

--- projects/ngx-carousel/src/public-api.ts
+++ projects/ngx-carousel/src/public-api.ts
@@ -1,9 +1,6 @@
 /*
  * Public API Surface of ngx-carousel
  */
-
-export * from './lib/ngx-carousel.service';
 export * from './lib/ngx-carousel.component';
-export * from './lib/ngx-carousel.module';
 export * from './lib/ngx-carousel-item/ngx-carousel-item.component';
 export * from './lib/ngx-carousel-item/ngx-carousel-item.model';
